5bc86842c9ad352fdda9c3c6ce7a1fdb1f39e640
Hadson Roque <nineonezero.light@gmail.com>
2023-04-04 11:50:58 +0200

    Create else with multiple function

--- PrimoProgetto/Program.cs
+++ PrimoProgetto/Program.cs
@@ -3,13 +3,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modifica");
 
 //variables
-var eta = 20;
+var eta = 15;
 
 //condition if your age is above +18
+
+    //posso scrivere questo blocco senza graffe se ho solo 1 comando
 if(eta>=18)
-{
-    Console.WriteLine("sei +18");
-} else
-{
+    Console.WriteLine("sei +18"); 
+
+    //qui ho bisogno delle graffe perche ho piu funzioni
+else
+{   
     Console.WriteLine("sei minorenne");
+    var rimanente = 18 - eta;
+    Console.WriteLine("ti mancano ancora " + rimanente + "anni");
 }
