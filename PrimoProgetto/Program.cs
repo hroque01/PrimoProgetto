0e8159f216c47cb519d2ecf041634fdc483710f5
Hadson Roque <nineonezero.light@gmail.com>
2023-04-04 11:47:19 +0200

    Add condition about variables age

--- PrimoProgetto/Program.cs
+++ PrimoProgetto/Program.cs
@@ -2,3 +2,14 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modifica");
 
+//variables
+var eta = 20;
+
+//condition if your age is above +18
+if(eta>=18)
+{
+    Console.WriteLine("sei +18");
+} else
+{
+    Console.WriteLine("sei minorenne");
+}
